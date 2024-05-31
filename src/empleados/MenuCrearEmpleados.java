22a948c2e10b00c7396515eead60332a788e62d8
Iván Ibáñez™ <ivan01092004@gmail.com>
2024-05-31 13:42:20 +0200

    Desarrollo de las pantallas de Empleado (En proceso)

--- src/empleados/MenuCrearEmpleados.java
+++ src/empleados/MenuCrearEmpleados.java
@@ -1,4 +1,18 @@
 package empleados;
 
+import javax.swing.*;
+
 public class MenuCrearEmpleados {
+    private JPanel panelCrearEmpleados;
+    private JLabel imgPromocion;
+    private JButton btnCancelar;
+    private JButton btnConfirmar;
+    private JTextField txtDni;
+    private JTextField txtNombre;
+    private JTextField txtApellidos;
+    private JTextField txtCorreo;
+    private JTextField txtTelefono;
+    private JTextField txtDireccion;
+    private JTextField txtFechaNacimiento;
+    private JTextField txtUsuario;
 }
