1ce0660b101e36586644add69ba812c3ee7a89a9
Iván Ibáñez™ <ivan01092004@gmail.com>
2024-06-05 12:24:49 +0200

    Managers de Compra, Venta y Producto

--- src/managers/CompraManager.java
+++ src/managers/CompraManager.java
@@ -1,9 +1,110 @@
 package managers;
 
+import DBManager.DBManager;
+import clientes.Cliente;
 import compras.Compra;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 public class CompraManager {
     public static ArrayList<Compra> compras = new ArrayList<>();
+
+    public static boolean getCompras(){
+        if (DBManager.connect()){
+            try(ResultSet rs = DBManager.getTableDataBase("SELECT * FROM COMPRAS")){
+                compras = new ArrayList<>();
+
+                while (rs.next()){
+                    int id = rs.getInt(1);
+                    Date fechaCompra = rs.getDate(2);
+                    int idProveedor = rs.getInt(3);
+                    int idProducto = rs.getInt(4);
+                    int cantidad = rs.getInt(5);
+                    double precioUnitario = rs.getDouble(6);
+                    Date fechaRecepcion = rs.getDate(7);
+                    int idEmpleado = rs.getInt(8);
+
+                    compras.add(new Compra(id,String.valueOf(fechaCompra), idProveedor, idProducto, cantidad, precioUnitario, String.valueOf(fechaRecepcion), idEmpleado));
+                }
+                return true;
+            } catch (SQLException e) {
+                e.printStackTrace();
+                return false;
+            }
+        }else {
+            return false;
+        }
+    }
+
+    public static List<Compra> getComprasList(){
+        return compras;
+    }
+
+    public static boolean anyadirCompra(Compra compra){
+        if (DBManager.connect() & DBManager.insertarCompras(compra)){
+            try {
+                compras.add(new Compra(compra.getId(), String.valueOf(compra.getFecha_compra()), compra.getId_proveedor(), compra.getId_producto(),
+                        compra.getCantidad(), compra.getPrecio_unitario(), String.valueOf(compra.getFecha_recepcion()), compra.getId_empleado()));
+            }catch (Exception ex){
+                ex.printStackTrace();
+                return false;
+            }finally {
+                DBManager.close();
+            }
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    public static boolean actualizarCompra(Compra compra){
+        try {
+            if (DBManager.connect()) {
+                if (DBManager.actualizarCompras(compra)) {
+                    borrarColumnaPorId(compra.getId());
+                    compras.add(compra);
+                    return true;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        } finally {
+            DBManager.close();
+        }
+        return false;
+    }
+
+    public static boolean eliminarCompra(Compra compra){
+        try {
+            if (DBManager.connect()) {
+                if (DBManager.eliminarCompra(compra.getId())) {
+                    borrarColumnaPorId(compra.getId());
+                    return true;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        } finally {
+            DBManager.close();
+        }
+        return false;
+    }
+
+    public static boolean borrarColumnaPorId(int id){
+        for (int i = 0; i < compras.size(); i++) {
+            if (compras.get(i).getId()==id){
+                compras.remove(i);
+                return true;
+            }
+        }
+        return false;
+    }
 }
--- src/managers/VentaManager.java
+++ src/managers/VentaManager.java
@@ -1,10 +1,109 @@
 package managers;
 
+import DBManager.DBManager;
+import compras.Compra;
 import ventas.Venta;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class VentaManager {
     public static List<Venta> ventas = new ArrayList<>();
+
+    public static boolean getCompras(){
+        if (DBManager.connect()){
+            try(ResultSet rs = DBManager.getTableDataBase("SELECT * FROM VENTAS")){
+                ventas = new ArrayList<>();
+
+                while (rs.next()){
+                    int id = rs.getInt(1);
+                    Date fechaVenta = rs.getDate(2);
+                    int cantidad = rs.getInt(3);
+                    double precioUnitario = rs.getDouble(4);
+                    int idCliente = rs.getInt(5);
+                    int idEmpleado = rs.getInt(6);
+                    int idProducto = rs.getInt(7);
+                    int idPromocion = rs.getInt(8);
+
+                    ventas.add(new Venta(id, String.valueOf(fechaVenta), cantidad, precioUnitario, idCliente, idEmpleado, idProducto, idPromocion));
+                }
+                return true;
+            } catch (SQLException e) {
+                e.printStackTrace();
+                return false;
+            }
+        }else {
+            return false;
+        }
+    }
+
+    public static List<Venta> getVentasList(){
+        return ventas;
+    }
+
+    public static boolean anyadirVenta(Venta venta){
+        if (DBManager.connect() & DBManager.insertarVentas(venta)){
+            try {
+                ventas.add(new Venta(venta.getId(), String.valueOf(venta.getFechaVenta()), venta.getCantidad(), venta.getPrecioUnitario(),
+                        venta.getIdCliente(), venta.getIdEmpleado(), venta.getIdProducto(), venta.getIdPromocion()));
+            }catch (Exception ex){
+                ex.printStackTrace();
+                return false;
+            }finally {
+                DBManager.close();
+            }
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    public static boolean actualizarVenta(Venta venta){
+        try {
+            if (DBManager.connect()) {
+                if (DBManager.actualizarVentas(venta)) {
+                    borrarColumnaPorId(venta.getId());
+                    ventas.add(venta);
+                    return true;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        } finally {
+            DBManager.close();
+        }
+        return false;
+    }
+
+    public static boolean eliminarVenta(Venta venta){
+        try {
+            if (DBManager.connect()) {
+                if (DBManager.eliminarVenta(venta.getId())) {
+                    borrarColumnaPorId(venta.getId());
+                    return true;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        } finally {
+            DBManager.close();
+        }
+        return false;
+    }
+
+    public static boolean borrarColumnaPorId(int id){
+        for (int i = 0; i < ventas.size(); i++) {
+            if (ventas.get(i).getId()==id){
+                ventas.remove(i);
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
